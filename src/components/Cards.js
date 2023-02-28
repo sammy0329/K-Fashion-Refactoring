877a2d92769270e4246ef80caa3479ea31aed6a5
sammy <sammy0329@naver.com>
2023-02-28 17:18:58 +0900

    Card 1행에 3개의 제품 들어가도록 설정 및 result.js에서 받은 값들을 Cards.js에서 처리하도록 분리

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,48 +1,49 @@
 // 카드들 노출시킬 컴포넌트
 
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row, Card, CardGroup } from "react-bootstrap";
 
-const Cards = (props) => {
+const Cards = ({ CardList }) => {
+  const columnPerRow = 3;
+
   return (
     <Container>
-      <div className="card mb-3" style={{ textAlign: "center" }}>
-        <h3 className="card-header">{props.productStore}</h3>
-        <div className="card-body">
-          <h5 className="card-title">{props.productName}</h5>
-          <h6 className="card-subtitle text-muted">Support card subtitle</h6>
-        </div>
+      <h4>추천 스타일 리스트</h4>
+      <hr />
+      <CardGroup>
+        <Row xs={1} md={columnPerRow}>
+          {CardList &&
+            CardList.map((product, idx) => (
+              <Col key={idx}>
+                <Card>
+                  <a
+                    href={product.productURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <Card.Img variant="top" src={product.productImg} />
+                  </a>
+                  <Card.Body>
+                    <Card.Title style={{color: "black"}}>{product.productName}</Card.Title>
 
-        <div className="card-body">
-          <div className="card_img">
-            <img
-              src={props.productImg}
-              alt="productImage"
-              width="100%"
-              height="auto"
-            />
-          </div>
-          <br />
-          <p className="card-text">
-            Price: ₩ {props.productPrice}
-            <br />
-            Category: {props.productCategory}
-          </p>
-        </div>
+                    <Card.Text>
+                      {/* 천원 단위로 콤마(,) 찍기 */}
+                      Price: ₩{" "}
+                      {product.productPrice
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      <br />
+                      Category: {product.productCategory}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
 
-        <div className="card-footer text-muted">
-          <div className="card-body">
-            {/* 해당 상품 URL로 이동하도록 onClick 이벤트 작성(새창으로 뜨게 구현함) */}
-            <button
-              type="button"
-              className="btn btn-info"
-              onClick={() => window.open(props.productURL)}
-            >
-              상품 보러 바로가기
-            </button>
-          </div>
-        </div>
-      </div>
+                <br />
+              </Col>
+            ))}
+        </Row>
+      </CardGroup>
     </Container>
   );
 };
