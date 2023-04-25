1a5aedca9db2aec2552c80b4298010a9c0725ed4
안성재 <anseongjae@anseongjaeui-MacBookAir.local>
2023-04-26 08:58:25 +0900

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
 	app.use(
 		createProxyMiddleware('/image', {
-			target: 'http://111.118.22.25:5717', 
+			target: '', 
 			changeOrigin: true,
 		})
 	);
